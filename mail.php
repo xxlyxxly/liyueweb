844f241dfc804643cdde35282d17651c523710a5
liyue <110769049+xxlyxxly@users.noreply.github.com>
2024-06-12 09:15:49 +0800

    Update mail.php
    add the email into the file

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "49976ly@sina.cn";
+$sitename = "liyue";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
